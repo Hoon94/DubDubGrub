99b914ae26f87cd8c7e10a3f1f264417dfd817f7
Hoon94 <dleognsdl1@naver.com>
2025-01-15 22:58:28 +0900

    feat: MapView - Show the Map

--- DubDubGrub/DubDubGrub/LocationMapView.swift
+++ DubDubGrub/DubDubGrub/LocationMapView.swift
@@ -5,11 +5,28 @@
 //  Created by Daehoon Lee on 1/15/25.
 //
 
+import MapKit
 import SwiftUI
 
 struct LocationMapView: View {
+    
+    @State private var position = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)))
+    
     var body: some View {
-        Text("Map View")
+        ZStack {
+            Map(position: $position)
+                .ignoresSafeArea()
+            
+            VStack {
+                Image(.ddgMapLogo)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 70)
+                    .shadow(radius: 10)
+                
+                Spacer()
+            }
+        }
     }
 }
 
