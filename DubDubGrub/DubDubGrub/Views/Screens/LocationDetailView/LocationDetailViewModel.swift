e7bce071991f96fb0b2c61e0b1d6313afff1f760
Hoon94 <dleognsdl1@naver.com>
2025-02-05 22:40:39 +0900

    feat: Open Maps For Directions & Make a Call

---
+++ DubDubGrub/DubDubGrub/Views/Screens/LocationDetailView/LocationDetailViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  LocationDetailViewModel.swift
+//  DubDubGrub
+//
+//  Created by Daehoon Lee on 2/5/25.
+//
+
+import MapKit
+import SwiftUI
+
+final class LocationDetailViewModel: ObservableObject {
+    
+    @Published var alertItem: AlertItem?
+    
+    let columns = [GridItem(.flexible()),
+                   GridItem(.flexible()),
+                   GridItem(.flexible())]
+    
+    var location: DDGLocation
+    
+    init(location: DDGLocation) {
+        self.location = location
+    }
+    
+    func getDirectionsToLocation() {
+        let placemark = MKPlacemark(coordinate: location.location.coordinate)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = location.name
+        
+        mapItem.openInMaps(launchOptions: [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeWalking])
+    }
+    
+    func callLocation() {
+        guard let url = URL(string: "tel://\(location.phoneNumber)") else {
+            alertItem = AlertContext.invalidPhoneNumber
+            return
+        }
+        
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url)
+        }
+    }
+}
