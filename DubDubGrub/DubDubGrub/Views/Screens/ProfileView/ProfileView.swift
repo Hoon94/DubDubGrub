6a2c6a24ee280ab6f1f58b2a5699b26acb6dd60e
Hoon94 <dleognsdl1@naver.com>
2025-02-14 15:47:24 +0900

    feat: ProfileView - Check Out

--- DubDubGrub/DubDubGrub/Views/Screens/ProfileView/ProfileView.swift
+++ DubDubGrub/DubDubGrub/Views/Screens/ProfileView/ProfileView.swift
@@ -36,7 +36,24 @@ struct ProfileView: View {
                 }
                 
                 VStack(alignment: .leading, spacing: 8) {
-                    CharactersRemainView(currentCount: viewModel.bio.count)
+                    HStack {
+                        CharactersRemainView(currentCount: viewModel.bio.count)
+                        Spacer()
+                        
+                        if viewModel.isCheckedIn {
+                            Button {
+                                viewModel.checkOut()
+                            } label: {
+                                Label("Check Out", systemImage: "mappin.and.ellipse")
+                                    .font(.system(size: 12, weight: .semibold))
+                                    .foregroundStyle(.white)
+                                    .padding(10)
+                                    .frame(height: 28)
+                                    .background(Color.grubRed)
+                                    .cornerRadius(8)
+                            }
+                        }
+                    }
                     
                     TextEditor(text: $viewModel.bio)
                         .frame(height: 100)
@@ -67,6 +84,7 @@ struct ProfileView: View {
         }
         .onAppear {
             viewModel.getProfile()
+            viewModel.getCheckedInStatus()
         }
         .alert(item: $viewModel.alertItem, content: { alertItem in
             Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
--- DubDubGrub/DubDubGrub/Views/Screens/ProfileView/ProfileViewModel.swift
+++ DubDubGrub/DubDubGrub/Views/Screens/ProfileView/ProfileViewModel.swift
@@ -11,13 +11,14 @@ enum ProfileContext { case create, update }
 
 final class ProfileViewModel: ObservableObject {
     
-    @Published var firstName    = ""
-    @Published var lastName     = ""
-    @Published var companyName  = ""
-    @Published var bio          = ""
-    @Published var avatar       = PlaceholderImage.avatar
+    @Published var firstName            = ""
+    @Published var lastName             = ""
+    @Published var companyName          = ""
+    @Published var bio                  = ""
+    @Published var avatar               = PlaceholderImage.avatar
     @Published var isShowingPhotoPicker = false
-    @Published var isLoading = false
+    @Published var isLoading            = false
+    @Published var isCheckedIn          = false
     @Published var alertItem: AlertItem?
     
     private var existingProfileRecord: CKRecord? {
@@ -35,6 +36,53 @@ final class ProfileViewModel: ObservableObject {
               bio.count <= 100 else { return false }
         
         return true
+    }
+    
+    func getCheckedInStatus() {
+        guard let profileRecordID = CloudKitManager.shared.profileRecordID else { return }
+        
+        CloudKitManager.shared.fetchRecord(with: profileRecordID) { [self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let record):
+                    if let _ = record[DDGProfile.kIsCheckedIn] as? CKRecord.Reference {
+                        isCheckedIn = true
+                    } else {
+                        isCheckedIn = false
+                    }
+                case .failure(_):
+                    break
+                }
+            }
+        }
+    }
+    
+    func checkOut() {
+        guard let profileID = CloudKitManager.shared.profileRecordID else {
+            alertItem = AlertContext.unableToGetProfile
+            return
+        }
+        
+        CloudKitManager.shared.fetchRecord(with: profileID) { result in
+            switch result {
+            case .success(let record):
+                record[DDGProfile.kIsCheckedIn] = nil
+                
+                CloudKitManager.shared.save(record: record) { [self] result in
+                    DispatchQueue.main.async {
+                        switch result {
+                        case .success(_):
+                            isCheckedIn = false
+                        case .failure(_):
+                            alertItem = AlertContext.unableToCheckInOrOut
+                        }
+                    }
+                }
+                
+            case .failure(_):
+                DispatchQueue.main.async { self.alertItem = AlertContext.unableToCheckInOrOut }
+            }
+        }
     }
     
     func createProfile() {
