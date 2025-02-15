f402aafe7c54185eef396becaa67ed69ce1c0c14
Hoon94 <dleognsdl1@naver.com>
2025-02-15 22:18:45 +0900

    feat: Custom Map Annotation - Implementation

---
+++ DubDubGrub/DubDubGrub/Views/CustomViews/Map Annotation/DDGAnnotation.swift
@@ -0,0 +1,45 @@
+//
+//  DDGAnnotation.swift
+//  DubDubGrub
+//
+//  Created by Daehoon Lee on 2/15/25.
+//
+
+import SwiftUI
+
+struct DDGAnnotation: View {
+    
+    var location: DDGLocation
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                MapBalloon()
+                    .frame(width: 100, height: 70)
+                    .foregroundStyle(.brandPrimary)
+                
+                Image(uiImage: location.createSquareImage())
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                    .offset(y: -11)
+                
+                Text("99")
+                    .font(.system(size: 11, weight: .bold))
+                    .frame(width: 26, height: 18)
+                    .background(Color.grubRed)
+                    .foregroundStyle(.white)
+                    .clipShape(Capsule())
+                    .offset(x: 20, y: -28)
+            }
+            
+            Text(location.name)
+                .font(.caption)
+                .fontWeight(.semibold)
+        }
+    }
+}
+
+#Preview {
+    DDGAnnotation(location: DDGLocation(record: MockData.location))
+}
