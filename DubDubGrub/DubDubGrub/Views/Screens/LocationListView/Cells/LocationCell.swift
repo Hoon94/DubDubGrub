b45a4e73c4bb3da005e3173eaf6c3ef974330fdf
Hoon94 <dleognsdl1@naver.com>
2025-02-15 21:39:49 +0900

    feat: LocationListView - Avatars - UI

---
+++ DubDubGrub/DubDubGrub/Views/Screens/LocationListView/Cells/LocationCell.swift
@@ -0,0 +1,73 @@
+//
+//  LocationCell.swift
+//  DubDubGrub
+//
+//  Created by Daehoon Lee on 1/22/25.
+//
+
+import SwiftUI
+
+struct LocationCell: View {
+    
+    var location: DDGLocation
+    var profiles: [DDGProfile]
+    
+    var body: some View {
+        HStack {
+            Image(uiImage: location.createSquareImage())
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+                .clipShape(Circle())
+                .padding(.vertical, 8)
+            
+            VStack(alignment: .leading) {
+                Text(location.name)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.75)
+                
+                if profiles.isEmpty {
+                    Text("Nobody's Checked In")
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.secondary)
+                        .padding(.top, 2)
+                } else {
+                    HStack {
+                        ForEach(profiles.indices, id: \.self) { index in
+                            if index <= 3 {
+                                AvatarView(image: profiles[index].createAvatarImage(), size: 35)
+                            } else if index == 4 {
+                                AdditionalProfilesView(number: profiles.count - 4)
+                            }
+                        }
+                    }
+                }
+            }
+            .padding(.leading)
+        }
+    }
+}
+
+#Preview {
+    Group {
+        LocationCell(location: DDGLocation(record: MockData.location), profiles: [])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
+
+struct AdditionalProfilesView: View {
+    
+    var number: Int
+    
+    var body: some View {
+        Text("+\(number)")
+            .font(.system(size: 14, weight: .semibold))
+            .frame(width: 35, height: 35)
+            .foregroundStyle(.white)
+            .background(Color.brandPrimary)
+            .clipShape(Circle())
+    }
+}
