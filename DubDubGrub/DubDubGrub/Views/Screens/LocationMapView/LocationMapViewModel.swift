730e7c44ad2187a30341d086910da475bf9a359e
Hoon94 <dleognsdl1@naver.com>
2025-02-17 22:47:04 +0900

    feat: Custom Map Annotation - Badges

--- DubDubGrub/DubDubGrub/Views/Screens/LocationMapView/LocationMapViewModel.swift
+++ DubDubGrub/DubDubGrub/Views/Screens/LocationMapView/LocationMapViewModel.swift
@@ -5,10 +5,12 @@
 //  Created by Daehoon Lee on 1/26/25.
 //
 
+import CloudKit
 import MapKit
 
 final class LocationMapViewModel: ObservableObject {
     
+    @Published var checkedInProfiles: [CKRecord.ID: Int] = [:]
     @Published var isShowingDetailView = false
     @Published var alertItem: AlertItem?
     @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054),
@@ -22,6 +24,19 @@ final class LocationMapViewModel: ObservableObject {
                     locationManager.locations = locations
                 case .failure(_):
                     alertItem = AlertContext.unableToGetLocations
+                }
+            }
+        }
+    }
+    
+    func getCheckedInCounts() {
+        CloudKitManager.shared.getCheckedInProfilesCount { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let checkedInProfiles):
+                    self.checkedInProfiles = checkedInProfiles
+                case .failure(_):
+                    break
                 }
             }
         }
