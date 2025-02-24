477220883ac3a6d3190d3d85c4d90399b20e0236
Hoon94 <dleognsdl1@naver.com>
2025-02-24 23:41:50 +0900

    feat: Dynamic Type - Implementation 2 - Modal

---
+++ DubDubGrub/DubDubGrub/Views/Screens/LocationDetailView/ProfileSheetView.swift
@@ -0,0 +1,49 @@
+//
+//  ProfileSheetView.swift
+//  DubDubGrub
+//
+//  Created by Daehoon Lee on 2/24/25.
+//
+
+import SwiftUI
+
+// Alternative Profile Modal View for larger dynamic type sizes
+// We present this as a sheet instead of a small pop up
+
+struct ProfileSheetView: View {
+    
+    var profile: DDGProfile
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 20) {
+                Image(uiImage: profile.createAvatarImage())
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 110, height: 110)
+                    .clipShape(Circle())
+                    .shadow(color: .black.opacity(0.5), radius: 4, x: 0, y: 6)
+                    .accessibilityHidden(true)
+                
+                Text(profile.firstName + " " + profile.lastName)
+                    .bold()
+                    .font(.title2)
+                    .minimumScaleFactor(0.9)
+                
+                Text(profile.companyName)
+                    .fontWeight(.semibold)
+                    .minimumScaleFactor(0.9)
+                    .foregroundStyle(.secondary)
+                    .accessibilityLabel(Text("Works at \(profile.companyName)"))
+                
+                Text(profile.bio)
+                    .accessibilityLabel(Text("Bio, \(profile.bio)"))
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ProfileSheetView(profile: DDGProfile(record: MockData.profile))
+}
